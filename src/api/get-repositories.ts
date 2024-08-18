f0ad5debf1eeae89e60bde18885ee30ba0c4b50a
Tânia Rocha <taniapassosrocha@gmail.com>
2024-08-18 17:00:40 +0100

    wip

--- src/api/get-repositories.ts
+++ src/api/get-repositories.ts
@@ -1,8 +1,6 @@
 import axios from "axios";
 import { RepositoryType } from "../types";
 
-const token = "";
-
 export const getRepositories = async (
   language: string
 ): Promise<RepositoryType[]> => {
@@ -13,7 +11,7 @@ export const getRepositories = async (
         headers: {
           Accept: "application/json",
           "Content-Type": "application/json",
-          Authorization: `Bearer ${token}`,
+          Authorization: `Bearer `,
         },
       }
     );
